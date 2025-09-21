cb032f1228d2ddb12fefbb91cd2288de6ce0b4c1
Emir <emirbesirx@gmail.com>
2025-09-21 08:39:50 +0300

    feat: add simple movement for player's car

---
+++ delivery-dash/Assets/_Game/Scripts/Driver.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float turnSpeed = 90f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+        }
+    }
+}
