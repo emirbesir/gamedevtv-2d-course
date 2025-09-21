614ebde322cc583e860974e8974ffae0ece91959
Emir <emirbesirx@gmail.com>
2025-09-21 09:38:39 +0300

    feat: add particle effects for package pickup and delivery

--- delivery-dash/Assets/_Game/Scripts/Delivery.cs
+++ delivery-dash/Assets/_Game/Scripts/Delivery.cs
@@ -3,17 +3,25 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     private bool hasPackage = false;
+    private ParticleSystem deliveryParticles;
+
+    private void Awake()
+    {
+        deliveryParticles = GetComponent<ParticleSystem>();
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Pickup"))
         {
             hasPackage = true;
+            deliveryParticles.Play();
             Destroy(col.gameObject);
         }
         if (col.CompareTag("Customer") && hasPackage)
         {
             hasPackage = false;
+            deliveryParticles.Stop();
             Destroy(col.gameObject);
         }
     }
