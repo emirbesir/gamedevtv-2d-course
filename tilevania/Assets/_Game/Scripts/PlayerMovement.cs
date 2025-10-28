8fa71dc7d8c2600a1814e03bfac114575be2bcba
emirbesir <emirbesirx@gmail.com>
2025-10-28 17:24:15 +0300

    feat: add climbing mechanic

--- tilevania/Assets/_Game/Scripts/PlayerMovement.cs
+++ tilevania/Assets/_Game/Scripts/PlayerMovement.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Config")]
-    [SerializeField] private float moveSpeed = 6f;
+    [Header("Walk Settings")]
+    [SerializeField] private float walkSpeed = 6f;
+    [Header("Jump Settings")]
     [SerializeField] private float jumpForce = 6f;
+    [Header("Climb Settings")]
+    [SerializeField] private float climbSpeed = 3f;
+    [SerializeField] private LayerMask climbingLayer;
     [Header("Ground Check Settings")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.15f;
@@ -16,21 +21,30 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 moveInput;
     private bool isMovingHorizontally;
+    private bool isMovingVertically;
+    private float gravityScaleAtStart;
 
     private Rigidbody2D rb;
+    private CapsuleCollider2D col;
     private SpriteRenderer rend;
     private Animator anim;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CapsuleCollider2D>();
+
         rend = playerVisual.GetComponent<SpriteRenderer>();
         anim = playerVisual.GetComponent<Animator>();
+
+        gravityScaleAtStart = rb.gravityScale;
     }
 
     private void FixedUpdate()
     {
-        Move();
+        Walk();
+        FlipSprite();
+        ClimbLadders();
     }
 
     private void OnMove(InputValue value)
@@ -38,14 +52,34 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
-    private void Move()
+    private void Walk()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocityY);
+        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocityY);
+
         isMovingHorizontally = Mathf.Abs(rb.linearVelocityX) > Mathf.Epsilon;
-
-        if (isMovingHorizontally) rend.flipX = moveInput.x < 0f;
-
         anim.SetBool(Constants.PlayerAnimations.IS_WALKING, isMovingHorizontally);
+    }
+    
+    private void FlipSprite()
+    {
+        if (isMovingHorizontally) rend.flipX = moveInput.x < 0f;
+    }
+
+    private void ClimbLadders()
+    {
+        if (!col.IsTouchingLayers(climbingLayer))
+        {
+            rb.gravityScale = gravityScaleAtStart;
+            anim.SetBool(Constants.PlayerAnimations.IS_CLIMBING, false);
+            return;
+        }
+
+        rb.gravityScale = 0f;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, moveInput.y * climbSpeed);
+
+        isMovingVertically = Mathf.Abs(rb.linearVelocityY) > Mathf.Epsilon;
+        anim.SetBool(Constants.PlayerAnimations.IS_CLIMBING, isMovingVertically);
     }
 
     private void OnJump()
